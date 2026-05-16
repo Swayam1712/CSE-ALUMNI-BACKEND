5880e142991be45632cd294813afdfacb8e50765
Milan Kumar Dandapat <milankumar07@outlook.com>
2026-05-16 18:45:13 +0530

    remove smtp secret

--- utils/emailService.js
+++ utils/emailService.js
@@ -9,13 +9,17 @@ import nodemailer from 'nodemailer';
  */
 const transporter = nodemailer.createTransport({
     host: 'smtp-relay.brevo.com',
-    port: 587,
+    port: 2525,
     secure: false,
 
     auth: {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASS,
     },
+
+    connectionTimeout: 20000,
+    greetingTimeout: 20000,
+    socketTimeout: 20000,
 });
 transporter.verify((error, success) => {
     if (error) {

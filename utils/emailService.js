216c2da70113d84f0a067af8eda8190af7807f7a
Milan Kumar Dandapat <milankumar07@outlook.com>
2026-05-16 19:16:03 +0530

    remove smtp secret

--- utils/emailService.js
+++ utils/emailService.js
@@ -8,8 +8,8 @@ import nodemailer from 'nodemailer';
  * SMTP_HOST, SMTP_PORT, SMTP_SECURE, SMTP_USER, SMTP_PASS
  */
 const transporter = nodemailer.createTransport({
-    host: 'smtp-relay.brevo.com',
-    port: 2525,
+    host: process.env.SMTP_HOST,
+    port: Number(process.env.SMTP_PORT),
     secure: false,
 
     auth: {
